86b33475b6bac604ea42231be940fb35398c7c97
anguyen221 <anguyen221@student.gsu.edu>
2025-03-26 13:53:33 -0400

    Testing firebase with sample product

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,8 +19,38 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Product Management App',
-      home: Scaffold(
-        body: Center(child: Text('Placeholder, but Firebase setup')),
+      home: ProductScreen(),
+    );
+  }
+}
+
+class ProductScreen extends StatefulWidget {
+  const ProductScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProductScreen> createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  final CollectionReference _products =
+      FirebaseFirestore.instance.collection('products');
+
+  Future<void> _addSampleProduct() async {
+    await _products.add({
+      "name": "Sample Product",
+      "price": 19.99,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Firestore Setup")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _addSampleProduct,
+          child: const Text("Add Sample Product"),
+        ),
       ),
     );
   }
